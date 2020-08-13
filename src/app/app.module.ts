17c7bc828ac19a782a78fb547713bc1a54c83b57
muellerluke <66047826+muellerluke@users.noreply.github.com>
2020-08-12 19:20:53 -0500

    3.8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { QuestionFormComponent } from './question-form/question-form.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ResultsComponent } from './results/results.component';
+import { RouterModule, Routes } from '@angular/router';
+import { QuestionsComponent } from './questions/questions.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: ':quizId', component: QuestionsComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
+];
 
 @NgModule({
-  declarations: [AppComponent, QuestionFormComponent],
-  imports: [BrowserModule, HttpClientModule, ReactiveFormsModule],
+  declarations: [
+    AppComponent,
+    QuestionFormComponent,
+    ResultsComponent,
+    QuestionsComponent,
+    WelcomeComponent,
+  ],
+  imports: [
+    RouterModule.forRoot(appRoutes),
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/question-form/question-form.component.ts
+++ src/app/question-form/question-form.component.ts
@@ -5,13 +5,13 @@ import { Question } from '../quiz.model';
 @Component({
   selector: 'app-question-form',
   templateUrl: './question-form.component.html',
-  styleUrls: ['./question-form.component.scss'],
+  styleUrls: ['./question-form.component.css'],
 })
 export class QuestionFormComponent implements OnInit {
   @Input() question: Question;
   @Output() onChoiceMade = new EventEmitter<string>();
 
-  private form: FormGroup;
+  form: FormGroup;
 
   ngOnInit() {
     this.form = new FormGroup({
